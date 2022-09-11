c54b40d849ffbf483554fda2a5565e29fd8701b8
PawanRoy <proy@teckborn.com>
2022-09-11 21:40:33 +0530

    Espresso test passed

--- app/src/androidTest/java/com/freeware/pdfconverter/activities/SplashActivityTest.kt
+++ app/src/androidTest/java/com/freeware/pdfconverter/activities/SplashActivityTest.kt
@@ -7,6 +7,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
