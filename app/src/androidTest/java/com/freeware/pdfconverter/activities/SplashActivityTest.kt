50ec0d14dea2419d4b49bd16b2966f8628916984
PawanRoy <proy@teckborn.com>
2022-09-11 20:16:32 +0530

    Jacoco Integrated

---
+++ app/src/androidTest/java/com/freeware/pdfconverter/activities/SplashActivityTest.kt
@@ -0,0 +1,24 @@
+package com.freeware.pdfconverter.activities
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class SplashActivityTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(SplashActivity::class.java)
+
+    @Test
+    fun seeIfActivityOpens(){
+        onView(withText("Splash Screen")).check(matches(isDisplayed()))
+    }
+}
---
+++ app/src/main/java/com/freeware/pdfconverter/activities/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.freeware.pdfconverter.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.freeware.pdfconverter.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
